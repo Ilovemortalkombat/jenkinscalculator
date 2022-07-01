6b0a6edadebf0448849511d026b05a2da9a98b2f
ajinkya <ajinkyabhutkar@gmail.com>
2022-07-01 10:01:40 +0530

    calculator program added

---
+++ JenkinsCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JenkinsCalculator {
+	public static void main(String args[])
+	{
+		int n1=15;
+                int n2=15;
+      
+             System.out.println("add= "+(n1+n2));
+              System.out.println("sub= "+(n1-n2));
+             System.out.println("mul= "+(n1*n2));
+              System.out.println("div= "+(n1/n2));
+ 
+ 
+	}
+	
+}
